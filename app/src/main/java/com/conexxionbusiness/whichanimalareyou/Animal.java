ebec9f53a19d62d2ea0371f2912f3f102603cf64
Confiuzer <conexxionbusiness@gmail.com>
2017-02-15 17:06:35 -0600

    Added Animal, Option , Question and QuestionBank classes.

---
+++ app/src/main/java/com/conexxionbusiness/whichanimalareyou/Animal.java
@@ -0,0 +1,23 @@
+package com.conexxionbusiness.whichanimalareyou;
+
+/**
+ * Created by IRVING on 15/02/2017.
+ */
+
+public class Animal {
+    private String mName;
+
+    public Animal(String name) {
+        this.mName = name;
+    }
+
+    public String getName() {
+        return mName;
+    }
+
+    public void setName(String name) {
+        this.mName = name;
+    }
+
+
+}
